b6976c9dd3388dc6ec730affc73a0e7d190ffd5d
t0pZ <kristoffer.laiz@outlook.com>
2026-05-02 20:40:41 +0800

    Revert "feat(park): add 10 animated park-decor elements + Park composition (Phase 6)"
    This reverts commit 90f67cdfa0725d5261dab8144e52ac9c197fe7bd.

--- src/game/world/areas/Park.tsx
+++ src/game/world/areas/Park.tsx
@@ -2,32 +2,32 @@
 import { Portal } from '@/src/game/world/Portal';
 import { Ground } from '@/src/game/world/props/Ground';
 import { Sky } from '@/src/game/world/props/Sky';
-import { Balloons } from './park-decor/Balloons';
-import { Bunny } from './park-decor/Bunny';
-import { Butterflies } from './park-decor/Butterfly';
-import { Clouds } from './park-decor/Clouds';
-import { Flowers } from './park-decor/Flowers';
-import { Kite } from './park-decor/Kite';
-import { Mushrooms } from './park-decor/Mushrooms';
-import { Pond } from './park-decor/Pond';
-import { Swing } from './park-decor/Swing';
-import { TreesAnimated } from './park-decor/TreesAnimated';
+
+const TREES: Array<[number, number]> = [
+  [3, 4],
+  [-4, 5],
+  [5, -3],
+  [-3, -4],
+  [-6, 0],
+];
 
 export function Park() {
   return (
     <>
       <Sky />
       <Ground color="#88c47a" />
-      <TreesAnimated />
-      <Flowers />
-      <Mushrooms />
-      <Pond />
-      <Swing />
-      <Clouds />
-      <Butterflies />
-      <Bunny />
-      <Kite />
-      <Balloons />
+      {TREES.map(([x, z], _i) => (
+        <group key={`${x}-${z}`} position={[x, 0, z]}>
+          <mesh position={[0, 0.5, 0]} castShadow>
+            <cylinderGeometry args={[0.15, 0.2, 1, 8]} />
+            <meshStandardMaterial color="#7a4a2c" />
+          </mesh>
+          <mesh position={[0, 1.4, 0]} castShadow>
+            <coneGeometry args={[0.7, 1.4, 8]} />
+            <meshStandardMaterial color="#3f8a3a" />
+          </mesh>
+        </group>
+      ))}
       <Portal to="town" position={[0, 0.7, 8]} />
     </>
   );
